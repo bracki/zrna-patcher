6916700a6c4ac1e3580d0b868f09f9e4381a66bc
Jan Brauer <jan.brauer@superluminar.io>
2019-09-07 20:10:40 +0200

    Make tray widget scrollable

--- src/components/BodyWidget.tsx
+++ src/components/BodyWidget.tsx
@@ -20,7 +20,7 @@ export const Body = styled.div`
 	flex-grow: 1;
 	display: flex;
 	flex-direction: column;
-	min-height: 100%;
+	height: 100%;
 `;
 
 export const Header = styled.div`
@@ -37,6 +37,7 @@ export const Header = styled.div`
 export const Content = styled.div`
 	display: flex;
 	flex-grow: 1;
+	height: 100%;
 `;
 
 export const Layer = styled.div`
