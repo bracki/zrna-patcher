592837a0ac8c94577a52f530a98b0f220090ab85
Jan Brauer <jan.brauer@superluminar.io>
2019-09-09 10:51:08 +0200

    Search for module

--- src/components/TrayWidget.tsx
+++ src/components/TrayWidget.tsx
@@ -9,8 +9,62 @@ export const Tray = styled.div`
 	overflow-y: auto;
 `;
 
-export class TrayWidget extends React.Component {
+
+export interface SearchBarProps {
+	filterText: string
+	onFilterTextChange(filterText: string): void 
+}
+
+export class SearchBar extends React.Component<SearchBarProps> {
+	constructor(props: SearchBarProps) {
+		super(props)
+		this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+	}
+
+	handleFilterTextChange(e: React.ChangeEvent<HTMLInputElement>) {
+		console.log("jasjasdjasdjasd");
+		console.log(e.target.value);
+		this.props.onFilterTextChange(e.target.value);
+	}
+
 	render() {
-		return <Tray>{this.props.children}</Tray>;
+		return (
+			<form>
+				<input
+					type="text"
+					placeholder="Search..."
+					value={this.props.filterText}
+					onChange={this.handleFilterTextChange}
+				/>
+			</form>
+		);
+	}
+}
+
+export interface TrayWidgetState {
+	filterText: string
+}
+
+export class TrayWidget extends React.Component<{}, TrayWidgetState> {
+	constructor(props: any) {
+		super(props);
+		this.state = {filterText: ''};
+		this.handleFilterTextChange = this.handleFilterTextChange.bind(this);
+	}
+
+	handleFilterTextChange(filterText: string) {
+		console.log("eulenbeule");
+		console.log(filterText);
+		this.setState({filterText: filterText});
+	}
+
+	render() {
+		const children = React.Children.toArray(this.props.children).filter((c: any) => {return c.props.name.startsWith(this.state.filterText)});
+		return (
+			<Tray>
+				<SearchBar filterText={this.state.filterText} onFilterTextChange={this.handleFilterTextChange} />
+				{children}
+			</Tray >
+		);
 	}
 }
